d30017e8d7169ca830e2ae23c7858517894e926c
Hediye Jamili <hediyejamili79@gmail.com>
2021-10-15 18:13:59 +0330

    code

---
+++ functional exercise/calculate your age.py
@@ -0,0 +1,41 @@
+import datetime
+
+
+def miladi_to_shamsi(gy, gm, gd,yy,mm,dd):
+ g_d_m = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
+ if (gm > 2):
+  gy2 = gy + 1
+ else:
+  gy2 = gy
+ days = 355666 + (365 * gy) + ((gy2 + 3) // 4) - ((gy2 + 99) // 100) + ((gy2 + 399) // 400) + gd + g_d_m[gm - 1]
+ jy = -1595 + (33 * (days // 12053))
+ days %= 12053
+ jy += 4 * (days // 1461)
+ days %= 1461
+ if (days > 365):
+  jy += (days - 1) // 365
+  days = (days - 1) % 365
+ if (days < 186):
+  jm = 1 + (days // 31)
+  jd = 1 + (days % 31)
+ else:
+  jm = 7 + ((days - 186) // 30)
+  jd = 1 + ((days - 186) % 30)
+ 
+ yy=jy-int(yy)
+ mm=jm-int(mm)
+ dd=jd-int(dd)
+
+ h=yy*52*7*24
+ min=h*60
+ sec=min*60
+
+ return print("you live for ", yy ," years  ", mm ," month ", dd," day ", h ," hours ",min," minutes and ",sec," seconds . ")
+
+x = datetime.datetime.now()
+y=x.year
+m=x.month
+d=x.day
+a=input("enter the date of your birthday : ( year month day )")
+yy,mm,dd=a.split("/")
+miladi_to_shamsi(y,m,d,yy,mm,dd)
