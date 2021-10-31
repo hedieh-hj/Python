96ffbc6b84125394838f6047cf2859a43528a7e2
Hediye Jamili <hediyejamili79@gmail.com>
2021-10-31 23:30:09 +0330

    folder

---
+++ store/store.py
@@ -0,0 +1,20 @@
+
+def show_menu():
+    print("""welcome
+    1.add
+    2.edit
+    3.remove
+    4.show list
+    5.search
+    6.buy
+    7.exit""")
+
+def add():
+    pass
+
+while 1: 
+    show_menu()
+    choice=int(input("\nplease enter your number : "))
+
+    if Choice ==1:
+        pass
